a332d30e43e13ef64c44f28aab889ccf3eb5573f
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2024-01-31 19:15:17 +0200

    Implement Post

---
+++ src/Post/Post.js
@@ -0,0 +1,45 @@
+import './Post.css'
+
+/**
+ * @typedef {object} User
+ * @prop {string} displayName
+ * @prop {string} imageURL
+ * @prop {string} username
+ */
+
+/**
+ * @typedef {object} PostDetails
+ * @prop {User} author
+ * @prop {string} contents
+ * @prop {string} timestamp
+ */
+
+/**
+ * @param {object} props
+ * @param {PostDetails} props.details
+ */
+function Post({ details }) {
+    const date = new Date(details.timestamp)
+    const timeStr = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+
+    return (
+        <div className="post border rounded">
+            <header className="d-flex flex-row p-2">
+                <img
+                    className="post-author-img"
+                    alt={'Profile picture of ' + details.author.displayName}
+                    src={details.author.imageURL}
+                />
+                <div className="d-flex flex-column text-start ms-3">
+                    <span className="post-author-name h5">
+                        {details.author.displayName}
+                    </span>
+                    <span className="post-time">Published on {timeStr}</span>
+                </div>
+            </header>
+            <article className="text-start p-3">{details.contents}</article>
+        </div>
+    )
+}
+
+export default Post
