5425d49762a2b92ad46f9673e64ce81f540e9fd9
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2024-01-31 23:34:20 +0200

    Post: add 'like', 'share' buttons and counters

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,4 +1,8 @@
 import './Post.css'
+import likeIcon from './like-icon.svg'
+import likeBtnWhite from './like-btn-white.svg'
+import likeBtnBlue from './like-btn-blue.svg'
+import shareBtn from './share-btn.svg'
 
 /**
  * @typedef {object} User
@@ -10,16 +14,42 @@ import './Post.css'
 /**
  * @typedef {object} PostDetails
  * @prop {User} author
- * @prop {string} contents
- * @prop {string} timestamp
+ * @prop {string} contents Text contents of the post
+ * @prop {string[]} likes An array containing the usernames of the users who liked the post
+ * @prop {number} shares An array containing the usernames of the users who shared the post
+ * @prop {string} timestamp Creation time
  */
 
 /**
  * @param {object} props
  * @param {PostDetails} props.details
+ * @param {(like: boolean) => void} props.toggleLike Will be called when the user clicks "Like"
+ * @param {Record<string, User>} props.users A map of all users
+ * @param {User} props.currentUser
  */
-function Post({ details }) {
+function Post({ currentUser, details, toggleLike, users }) {
+    /**
+     * @param {string[]} names
+     * @param {Record<string, User>} users
+     */
+    function usernamesToStr(names) {
+        names = names.map(username =>
+            username === currentUser.username
+                ? 'You'
+                : users[username].displayName,
+        )
+        if (names.length === 0) return ''
+        if (names.length === 1) return names[0]
+        if (names.length === 2) return names[0] + ' and ' + names.at(-1)
+        if (names.length > 2) {
+            return names.slice(0, -1).join(', ') + ', and ' + names.at(-1)
+        }
+    }
+
     const date = new Date(details.timestamp)
+    const likesNameList = usernamesToStr(details.likes, users)
+    const sharesNameList = usernamesToStr(details.shares, users) // Empty for now
+    const isLikedByMe = details.likes.includes(currentUser.username)
 
     return (
         <div className="post border rounded mb-3">
@@ -41,6 +71,52 @@ function Post({ details }) {
                 </div>
             </header>
             <article className="text-start p-3">{details.contents}</article>
+            <footer className="border-top container">
+                <div className="row m-2">
+                    <span className="likes-count col text-start ps-3 tooltip-container">
+                        <img
+                            className="post-like-icon icon-link m-1"
+                            src={likeIcon}
+                            alt="likes"
+                        />
+                        <span className="align-middle">
+                            {details.likes.length}
+                        </span>
+                        {details.likes.length > 0 && (
+                            <span className="tooltip-text rounded p-1">
+                                {likesNameList} liked this post
+                            </span>
+                        )}
+                    </span>
+                    <span className="shares-count col text-end pe-4 align-middle tooltip-container">
+                        {details.shares.length} shares
+                        {details.shares.length > 0 && (
+                            <span className="tooltip-text rounded p-1">
+                                {sharesNameList} shared this post
+                            </span>
+                        )}
+                    </span>
+                </div>
+                <div className="post-actions-row row border-top m-2">
+                    <span className="col">
+                        <button
+                            className="btn icon-link"
+                            onClick={() => toggleLike(!isLikedByMe)}>
+                            <img
+                                src={isLikedByMe ? likeBtnBlue : likeBtnWhite}
+                                alt="Like the post"
+                            />
+                            Like
+                        </button>
+                    </span>
+                    <span className="col">
+                        <button className="btn icon-link">
+                            <img src={shareBtn} alt="Share the post" />
+                            Share
+                        </button>
+                    </span>
+                </div>
+            </footer>
         </div>
     )
 }
