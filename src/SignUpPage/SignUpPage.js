2b0160e79b7530fb1e8a7f46d745762a89b262b9
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2024-02-15 21:57:28 +0200

    Final touches

--- src/SignUpPage/SignUpPage.js
+++ src/SignUpPage/SignUpPage.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState, useRef } from 'react'
+import './SignUpPage.css'
 import { useNavigate } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import AdvancedTextField from '../TextField/AdvancedTextField.js'
 import { UserContent } from '../App/App.js'
+import AdvancedTextField from '../TextField/AdvancedTextField.js'
+import React, { useContext, useState, useRef } from 'react'
 
 function SignUpPage() {
     const [username, setUsername] = useState('')
@@ -33,10 +33,6 @@ function SignUpPage() {
             alert('One or more of the fields are invalid')
         }
     }
-    //setting the profile image
-    const handleImageChange = selectedImage => {
-        setImg(selectedImage)
-    }
 
     const handleSignInPageClick = () => {
         navigate('/signin')
@@ -44,148 +40,119 @@ function SignUpPage() {
 
     const handleAddPictureClick = () => {
         // Trigger the hidden file input and enabling us to add a picture using reference
-        if (fileInputRef.current) {
-            fileInputRef.current.click()
-        }
+        fileInputRef.current.click()
     }
+
     //checks if all the input is valid
-    const checkAllValid = () => {
-        return (
-            profileImage != null &&
-            isUsernameValid(username) &&
-            isPasswordValid(password) &&
-            isRePasswordValid(rePassword) &&
-            isDisplayNameValid(displayName)
-        )
-    }
+    const checkAllValid = () =>
+        profileImage != null &&
+        isUsernameValid(username) &&
+        isPasswordValid(password) &&
+        isRePasswordValid(rePassword) &&
+        isDisplayNameValid(displayName)
+
     //functions to check if the input by the user is valid
-    const isUsernameValid = username => {
-        return checkLen(username, 5, 16) && isOnlyLettersAndNumbers(username)
-    }
 
-    const isPasswordValid = password => {
-        return checkLen(password, 8, 16) && isOnlyLettersAndNumbers(password)
-    }
+    const isOnlyLettersAndNumbers = item => /^[a-zA-Z0-9]+$/.test(item)
+    const isOnlyLettersAndNumbersAndSpace = item =>
+        /^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$/.test(item)
 
-    const isRePasswordValid = rePassword => {
-        return rePassword === password
-    }
+    const isUsernameValid = username =>
+        checkLen(username, 5, 16) && isOnlyLettersAndNumbers(username)
 
-    const isDisplayNameValid = displayName => {
-        return (
-            checkLen(displayName, 2, 16) &&
-            isOnlyLettersAndNumbersAndSpace(displayName)
-        )
-    }
+    const isPasswordValid = password =>
+        checkLen(password, 8, 16) && isOnlyLettersAndNumbers(password)
 
-    const checkLen = (item, min, max) => {
-        return item.length >= min && item.length <= max
-    }
+    const isRePasswordValid = rePassword => rePassword === password
 
-    const isOnlyLettersAndNumbers = item => {
-        return /^[a-zA-Z0-9]+$/.test(item)
-    }
-    const isOnlyLettersAndNumbersAndSpace = item => {
-        return /^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$/.test(item)
-    }
+    const isDisplayNameValid = isOnlyLettersAndNumbersAndSpace
+
+    const checkLen = (item, min, max) =>
+        item.length >= min && item.length <= max
+
     //file input reference, used to save the picture by reference
     const fileInputRef = useRef(null)
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'flex-start', // Adjust this as needed
-                minHeight: '80vh',
-                marginTop: '50px', // Now it's a minimum height, allowing growth
-            }}>
+        <div className="d-flex flex-column justify-content-center align-items-center mt-5">
             <header className="bg-primary text-white p-3">
                 <h1>Sign Up</h1>
             </header>
-            {/* Username textField */}
-            <AdvancedTextField
-                label="Username"
-                onInputChange={userNameValue => setUsername(userNameValue)}
-                textFieldId={'signUpUsername'}
-                funcValid={isUsernameValid}
-                inValidationErrorMessage={'Invalid Username'}
-                instruction={'English letters and numbers only, 5-16 letters'}
-                isMasked={false}
-            />
-            {/* Password textField */}
-            <AdvancedTextField
-                label="Password"
-                onInputChange={passwordValue => setPassword(passwordValue)}
-                textFieldId={'signUpPassword'}
-                funcValid={isPasswordValid}
-                inValidationErrorMessage={'Invalid Password'}
-                instruction={'English letters and numbers only, 8-16 letters'}
-                isMasked={true}
-            />
-            {/* re-type password textField */}
-            <AdvancedTextField
-                label="Re-type password"
-                onInputChange={reTypePassword => setRePassword(reTypePassword)}
-                textFieldId={'signUpReTypePassword'}
-                funcValid={isRePasswordValid}
-                inValidationErrorMessage={'Invalid Password'}
-                instruction={'Re-type the password'}
-                isMasked={true}
-            />
-            {/* Add profile picture button */}
-            <div style={{ color: 'red', fontSize: 'small' }}>
-                {'*Add a profile picture (must)'}
-            </div>
-            <div style={{ margin: '5px 0' }}>
+
+            <div className="d-flex flex-column gap-2">
+                <AdvancedTextField
+                    label="Username"
+                    onInputChange={userNameValue => setUsername(userNameValue)}
+                    funcValid={isUsernameValid}
+                    inValidationErrorMessage="Invalid Username"
+                    instruction="5-16 alphanumeric characters"
+                    isMasked={false}
+                />
+                <AdvancedTextField
+                    label="Password"
+                    onInputChange={passwordValue => setPassword(passwordValue)}
+                    funcValid={isPasswordValid}
+                    inValidationErrorMessage="Invalid Password"
+                    instruction="8-16 alphanumeric characters"
+                    isMasked={true}
+                />
+                <AdvancedTextField
+                    label="Re-type password"
+                    onInputChange={reTypePassword =>
+                        setRePassword(reTypePassword)
+                    }
+                    funcValid={isRePasswordValid}
+                    inValidationErrorMessage="Invalid Password"
+                    instruction="Re-type the password"
+                    isMasked={true}
+                />
+                <span
+                    className="text-center"
+                    style={{ color: 'red', fontSize: 'small' }}>
+                    * Add a profile picture (must)
+                </span>
                 <button
-                    className="btn btn-success"
+                    className="btn btn-success mx-auto"
                     onClick={handleAddPictureClick}>
                     Add Picture
                 </button>
                 <input
-                    type="file"
                     accept="image/*"
-                    style={{ display: 'none' }}
-                    onChange={e => handleImageChange(e.target.files[0])}
+                    className="d-none"
+                    onChange={e => setImg(e.target.files[0] ?? null)}
                     ref={fileInputRef}
+                    type="file"
                 />
-                {/* Shows up the profile image */}
-            </div>
-            {profileImage && (
-                <>
-                    <h2>Your Profile Picture</h2>
-                    <img
-                        src={URL.createObjectURL(profileImage)}
-                        alt="Profile"
-                        style={{ width: '500px', height: 'auto' }}
-                    />
-                </>
-            )}
-            {/* Display name textField */}
-            <AdvancedTextField
-                label="Display name"
-                onInputChange={displayNameValue =>
-                    setDisplayName(displayNameValue)
-                }
-                textFieldId={'signUpDisplayName'}
-                funcValid={isDisplayNameValid}
-                inValidationErrorMessage={'Invalid Display name'}
-                instruction={'English letters and numbers only, 2-16 letters'}
-            />
-            {/* Sign up button */}
-            <div style={{ margin: '5px 0' }}>
-                <button className="btn btn-primary" onClick={handleSignUpClick}>
-                    Sign-up
+                {profileImage && (
+                    <>
+                        <h2>Your Profile Picture</h2>
+                        <img
+                            className="profile-image-preview"
+                            src={URL.createObjectURL(profileImage)}
+                            alt="Profile"
+                        />
+                    </>
+                )}
+                <AdvancedTextField
+                    label="Display name"
+                    onInputChange={displayNameValue =>
+                        setDisplayName(displayNameValue)
+                    }
+                    funcValid={isDisplayNameValid}
+                    inValidationErrorMessage="Invalid Display name"
+                    instruction="2-16 alphanumeric characters"
+                />
+                <button
+                    className="btn btn-primary mt-3 mx-auto mb-1"
+                    onClick={handleSignUpClick}>
+                    Sign Up
                 </button>
             </div>
-            {/* Sign in page button */}
             <div style={{ margin: '5px 0' }}>
                 <button
-                    className="btn btn-primary"
+                    className="btn btn-danger"
                     onClick={handleSignInPageClick}>
-                    Sign-in page
+                    Sign In Page
                 </button>
             </div>
         </div>
