cbb41a1b2074dbf760ae4a605ae9ede438aa29e3
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2024-03-03 19:35:18 +0200

    Use localstorage for JWT

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,16 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:8080/api/'
 
+const getJWT = () => {
+    const token = localStorage.getItem('jwtToken')
+    return 'Bearer ' + token
+}
+
 const instance = axios.create({
     baseURL: BASE_URL,
+    headers: {
+        Authorization: getJWT(),
+    },
 })
 
 export default instance
